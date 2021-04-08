95e6184bab97553a9eada128939827a60ee9448e
Md. Asaduzzaman <md.asad.mony@gmail.com>
2021-04-09 01:33:21 +0600

    react dom router implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/pages/Home'
+import ProductDetails from './components/pages/ProductDetails';
 function App() {
   return (
-    <Home></Home>
+    <BrowserRouter>
+    <Route path="/" component={Home} exact></Route>
+    <Route path="/product/:id" component={ProductDetails}></Route>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/pages/ProductDetails.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ProductDetails(props) {
+    const {product} = props
+    return (
+        <div>
+            product details {product}
+        </div>
+    )
+}
